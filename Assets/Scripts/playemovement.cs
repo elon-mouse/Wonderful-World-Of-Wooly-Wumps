6a97dee0a81baa26b4cd0b8d8b06e37501ab70bc
elon-mouse <ahkn89@gmail.com>
2026-01-03 17:03:59 -0500

    fixed pc movement

--- Assets/Scripts/playemovement.cs
+++ Assets/Scripts/playemovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontal = VirtualJoystick.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal");
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
